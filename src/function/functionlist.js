721d95b010f11b9c27f2622a0090f38f5863887d
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2022-01-12 00:48:01 +0000

    load custom functions

--- src/function/functionlist.js
+++ src/function/functionlist.js
@@ -1,9 +1,8 @@
 import functionImplementation from './functionImplementation';
 import Store from '../store/index'
 import locale from '../locale/locale';
-//{"0":"数学","1":"统计","2":"查找","3":"Luckysheet内置","4":"数据挖掘","5":"数据源","6":"日期","7":"过滤器","8":"财务","9":"工程计算","10":"逻辑","11":"运算符","12":"文本","13":"转换工具","14":"数组"}
 
-const functionlist = function(){
+const functionlist = function () {
     let _locale = locale();
     // internationalization,get function list
     let functionListOrigin = _locale.functionlist;
@@ -11,11 +10,18 @@ const functionlist = function(){
     // add new property f
     for (let i = 0; i < functionListOrigin.length; i++) {
         let func = functionListOrigin[i];
-        func.f = functionImplementation[func.n];
+        if (functionImplementation[func.n]) {
+            func.f = functionImplementation[func.n];
+        }
     }
-
-    Store.functionlist = functionListOrigin;
-    
+    if (window.customFunctions) {
+        for (let i = 0; i < functionListOrigin.length; i++) {
+            let func = functionListOrigin[i];
+            if (window.customFunctions[func.n]) {
+                func.f = window.customFunctions[func.n];
+            }
+        }
+    }
     // get n property
     const luckysheet_function = {};
 
@@ -24,8 +30,10 @@ const functionlist = function(){
         luckysheet_function[func.n] = func;
     }
 
+    Store.functionlist = functionListOrigin;
+
     window.luckysheet_function = luckysheet_function; //Mount window for eval() calculation formula
-    
+
     Store.luckysheet_function = luckysheet_function;
 }
 
--- src/locale/locale.js
+++ src/locale/locale.js
@@ -1,10 +1,14 @@
 import en from './en'
 import Store from '../store';
-
-const localeObj = { en }
-
+let combined = { en };
+let storedFunctions = window.customFunctionList;
 function locale() {
-    return localeObj[Store.lang];
+    if (!storedFunctions) {
+        storedFunctions = window.customFunctionList;
+        if (storedFunctions)
+            combined[Store.lang]["functionlist"].push(...storedFunctions["customFunctionList"]);
+    }
+    return combined[Store.lang];
 }
 
 export default locale;
